bcfbad51988600fd4a2e3221c5e9cc7ecd2704d8
Momoy369 <ronijagat@gmail.com>
2025-03-27 10:24:05 +0800

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Aktifkan CORS
+  app.enableCors({
+    origin: 'https://maddog-token.site',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
