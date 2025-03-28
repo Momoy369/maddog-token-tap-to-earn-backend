358ade18a3f3b07d45e04a8e6a981cfde34b5085
Momoy369 <ronijagat@gmail.com>
2025-03-29 05:48:40 +0800

    adad

--- typeorm.config.ts
+++ typeorm.config.ts
@@ -1,12 +1,12 @@
-// import { DataSource } from 'typeorm';
-// import 'dotenv/config'; // Pastikan env bisa dibaca
+import { DataSource } from 'typeorm';
+import 'dotenv/config'; // Pastikan env bisa dibaca
 
-// export const dataSource = new DataSource({
-//   type: 'postgres',
-//   url: process.env.DATABASE_URL,
-//   entities: ['src/**/*.entity{.ts,.js}'],
-//   migrations: ['src/migrations/*.ts'],
-//   synchronize: true,
-// });
+export const dataSource = new DataSource({
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  entities: ['src/**/*.entity{.ts,.js}'],
+  migrations: ['src/migrations/*.ts'],
+  synchronize: true,
+});
 
-// export default dataSource;
+export default dataSource;
