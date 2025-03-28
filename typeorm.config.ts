d49877f1bbae123f078665a23de777a855ad6c74
Momoy369 <ronijagat@gmail.com>
2025-03-29 05:10:10 +0800

    migrations dir

--- typeorm.config.ts
+++ typeorm.config.ts
@@ -4,8 +4,8 @@ import 'dotenv/config'; // Pastikan env bisa dibaca
 export const dataSource = new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  entities: ['src/**/*.entity{.ts,.js}'], // Pastikan entity masuk
-  migrations: ['src/migrations/*.ts'], // Lokasi file migration
+  entities: ['src/**/*.entity{.ts,.js}'],
+  migrations: ['src/migrations/*.ts'],
   synchronize: false,
 });
 
